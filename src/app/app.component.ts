94780e0a624b66f46859afc06fe8dec481d6e670
Wanner <dwanner@Cardinals-MacBook-Pro.local>
2017-08-11 08:49:22 -0400

    added a little change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'delta angular2 works!';
+  title = 'delta angular2 works - starting work!';
 }
